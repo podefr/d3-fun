f0792e1f4f9600e504977021f0c6c54fa51ab9c4
Olivier Scherrer <pode.fr@gmail.com>
2014-06-23 23:57:34 -0400

    add skeleton

--- js/line-chart.js
+++ js/line-chart.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var d3 = require("d3");
+
 module.exports = function LineChart() {
     this.ySeries = function (data, legend) {
         return this;
@@ -11,5 +13,13 @@ module.exports = function LineChart() {
 
     this.addLine = function (data, legend) {
         return this;
+    };
+
+    this.place = function (dom) {
+
+    };
+
+    this.render = function () {
+
     };
 };
